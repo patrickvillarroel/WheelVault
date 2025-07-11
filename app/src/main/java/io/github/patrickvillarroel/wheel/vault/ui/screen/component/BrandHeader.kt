7e3222096e13daa549189238a4fcfdca19ab2e94
Rey <89825425+kingg22@users.noreply.github.com>
2025-07-11 13:26:49 -0500

    feat(detail): add brand detail and brand header

--- app/src/main/java/io/github/patrickvillarroel/wheel/vault/ui/screen/component/BrandHeader.kt
+++ app/src/main/java/io/github/patrickvillarroel/wheel/vault/ui/screen/component/BrandHeader.kt
@@ -1,11 +1,12 @@
 package io.github.patrickvillarroel.wheel.vault.ui.screen.component
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.KeyboardArrowLeft
@@ -17,20 +18,25 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.github.patrickvillarroel.wheel.vault.R
 import io.github.patrickvillarroel.wheel.vault.ui.theme.WheelVaultTheme
 
 @Composable
-fun MenuHeader(onBackClick: () -> Unit, onProfileClick: () -> Unit, modifier: Modifier = Modifier) {
+fun BrandHeader(
+    icon: Pair<Int, String?>,
+    onBackClick: () -> Unit,
+    onProfileClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
     Box(
         modifier = modifier
             .fillMaxWidth()
-            .height(75.dp)
+            .height(292.dp)
             .background(
                 color = Color(0xFFE42E31),
                 shape = RoundedCornerShape(
@@ -41,26 +47,33 @@ fun MenuHeader(onBackClick: () -> Unit, onProfileClick: () -> Unit, modifier: Mo
                 ),
             ),
     ) {
-        MenuButtonHeader(onProfileClick = onProfileClick) {
-            Text(
-                stringResource(R.string.collectors_project),
-                textAlign = TextAlign.Center,
-                fontWeight = FontWeight.Bold,
-                style = MaterialTheme.typography.titleLarge,
-                modifier = Modifier.align(Alignment.CenterVertically).wrapContentWidth(),
-            )
-        }
-        TextButton(onBackClick, modifier = Modifier.padding(top = 40.dp, start = 15.dp)) {
+        MenuButtonHeader(onProfileClick = onProfileClick)
+        Image(
+            painterResource(icon.first),
+            icon.second,
+            Modifier.align(Alignment.Center).width(300.dp),
+            contentScale = ContentScale.Crop,
+        )
+        TextButton(
+            onBackClick,
+            modifier = Modifier
+                .align(Alignment.BottomStart)
+                .padding(top = 40.dp, start = 15.dp),
+        ) {
             Icon(Icons.AutoMirrored.Default.KeyboardArrowLeft, stringResource(R.string.back), tint = Color.Black)
-            Text(stringResource(R.string.back), color = Color.Black)
+            Text(stringResource(R.string.back), color = Color.Black, style = MaterialTheme.typography.labelLarge)
         }
     }
 }
 
 @Preview
 @Composable
-private fun HeaderBrushPreview() {
+private fun BrandHeaderPreview() {
     WheelVaultTheme {
-        MenuHeader(onBackClick = {}, onProfileClick = {})
+        BrandHeader(
+            R.drawable.hot_wheels_logo_black to "Hot Wheels Logo",
+            onBackClick = {},
+            onProfileClick = {},
+        )
     }
 }
