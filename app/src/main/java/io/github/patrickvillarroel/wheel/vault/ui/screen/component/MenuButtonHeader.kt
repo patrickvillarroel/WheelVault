bdb036aac29ae58ca852eb73f2f45980493c6421
Rey <89825425+Kingg22@users.noreply.github.com>
2025-07-12 13:48:23 -0500

    perf(components): move to single component menu drop down

--- app/src/main/java/io/github/patrickvillarroel/wheel/vault/ui/screen/component/MenuButtonHeader.kt
+++ app/src/main/java/io/github/patrickvillarroel/wheel/vault/ui/screen/component/MenuButtonHeader.kt
@@ -1,26 +1,13 @@
 package io.github.patrickvillarroel.wheel.vault.ui.screen.component
 
-import androidx.compose.animation.AnimatedContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.filled.ViewWeek
-import androidx.compose.material.icons.outlined.Calculate
-import androidx.compose.material.icons.outlined.DirectionsCar
-import androidx.compose.material.icons.outlined.Favorite
-import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
@@ -37,50 +24,15 @@ fun MenuButtonHeader(
     modifier: Modifier = Modifier,
     title: @Composable () -> Unit = {},
 ) {
-    var expandedMenu by remember { mutableStateOf(false) }
-
     TopAppBar(
         modifier = modifier,
         title = title,
         navigationIcon = {
-            IconButton({ expandedMenu = !expandedMenu }) {
-                AnimatedContent(expandedMenu, label = "Menu Swip") {
-                    Icon(
-                        imageVector = if (!it) Icons.Default.Menu else Icons.Default.ViewWeek,
-                        contentDescription = stringResource(R.string.menu),
-                        tint = Color.White,
-                    )
-                }
-            }
-            DropdownMenu(
-                expanded = expandedMenu,
-                onDismissRequest = { expandedMenu = false },
-            ) {
-                DropdownMenuItem(
-                    text = { Text(stringResource(R.string.garage)) },
-                    leadingIcon = { Icon(Icons.Outlined.DirectionsCar, contentDescription = null) },
-                    onClick = {
-                        expandedMenu = false
-                        onGarageClick()
-                    },
-                )
-                DropdownMenuItem(
-                    text = { Text(stringResource(R.string.favorites)) },
-                    leadingIcon = { Icon(Icons.Outlined.Favorite, contentDescription = null) },
-                    onClick = {
-                        expandedMenu = false
-                        onFavoritesClick()
-                    },
-                )
-                DropdownMenuItem(
-                    text = { Text(stringResource(R.string.statistics)) },
-                    leadingIcon = { Icon(Icons.Outlined.Calculate, contentDescription = null) },
-                    onClick = {
-                        expandedMenu = false
-                        onStatisticsClick()
-                    },
-                )
-            }
+            MenuDropDown(
+                onGarageClick = onGarageClick,
+                onFavoritesClick = onFavoritesClick,
+                onStatisticsClick = onStatisticsClick,
+            )
         },
         actions = {
             IconButton(onClick = onProfileClick) {
---
+++ app/src/main/java/io/github/patrickvillarroel/wheel/vault/ui/screen/component/MenuDropdown.kt
@@ -0,0 +1,85 @@
+package io.github.patrickvillarroel.wheel.vault.ui.screen.component
+
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.ViewWeek
+import androidx.compose.material.icons.outlined.Calculate
+import androidx.compose.material.icons.outlined.DirectionsCar
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import io.github.patrickvillarroel.wheel.vault.R
+import io.github.patrickvillarroel.wheel.vault.ui.theme.WheelVaultTheme
+
+@Suppress("ktlint:compose:multiple-emitters-check")
+@Composable
+fun MenuDropDown(
+    onGarageClick: () -> Unit,
+    onFavoritesClick: () -> Unit,
+    onStatisticsClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    var expandedMenu by remember { mutableStateOf(false) }
+    IconButton({ expandedMenu = !expandedMenu }, modifier = modifier) {
+        AnimatedContent(expandedMenu, label = "Menu Swip") {
+            Icon(
+                imageVector = if (!it) Icons.Default.Menu else Icons.Default.ViewWeek,
+                contentDescription = stringResource(R.string.menu),
+                tint = Color.White,
+            )
+        }
+    }
+    DropdownMenu(
+        expanded = expandedMenu,
+        onDismissRequest = { expandedMenu = false },
+    ) {
+        DropdownMenuItem(
+            text = { Text(stringResource(R.string.garage)) },
+            leadingIcon = { Icon(Icons.Outlined.DirectionsCar, contentDescription = null) },
+            onClick = {
+                expandedMenu = false
+                onGarageClick()
+            },
+        )
+        DropdownMenuItem(
+            text = { Text(stringResource(R.string.favorites)) },
+            leadingIcon = { Icon(Icons.Outlined.Favorite, contentDescription = null) },
+            onClick = {
+                expandedMenu = false
+                onFavoritesClick()
+            },
+        )
+        DropdownMenuItem(
+            text = { Text(stringResource(R.string.statistics)) },
+            leadingIcon = { Icon(Icons.Outlined.Calculate, contentDescription = null) },
+            onClick = {
+                expandedMenu = false
+                onStatisticsClick()
+            },
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun HeaderBrushPreview() {
+    WheelVaultTheme {
+        Column {
+            MenuDropDown(onGarageClick = {}, onFavoritesClick = {}, onStatisticsClick = {})
+        }
+    }
+}
