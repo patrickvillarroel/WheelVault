70237666a587a902b7b84f4db7aaca3a284b5b03
Rey <89825425+Kingg22@users.noreply.github.com>
2025-07-12 16:14:42 -0500

    perf(component): add if resource drawable or async image

--- app/src/main/java/io/github/patrickvillarroel/wheel/vault/ui/screen/component/BrandCard.kt
+++ app/src/main/java/io/github/patrickvillarroel/wheel/vault/ui/screen/component/BrandCard.kt
@@ -1,38 +1,48 @@
 package io.github.patrickvillarroel.wheel.vault.ui.screen.component
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import io.github.patrickvillarroel.wheel.vault.R
 
 @Composable
-fun BrandCard(@DrawableRes logo: Int, onClick: () -> Unit, modifier: Modifier = Modifier) {
-    Box(
-        modifier = modifier
-            .size(80.dp)
-            .clip(RoundedCornerShape(12.dp))
-            .background(Color(0xFFE53935))
-            .clickable(onClick = onClick),
-        contentAlignment = Alignment.Center,
+fun BrandCard(logo: Any, onClick: () -> Unit, modifier: Modifier = Modifier) {
+    Card(
+        onClick = onClick,
+        modifier = modifier.size(80.dp),
+        shape = RoundedCornerShape(12.dp),
+        colors = CardDefaults.cardColors(containerColor = Color(0xFFE53935)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp),
     ) {
-        Image(
-            painter = painterResource(id = logo),
-            contentDescription = stringResource(R.string.car),
-            modifier = Modifier.size(40.dp),
-            contentScale = ContentScale.Fit,
-        )
+        Box(contentAlignment = Alignment.Center, modifier = Modifier.fillMaxSize()) {
+            if (logo is Painter) {
+                Image(
+                    painter = logo,
+                    contentDescription = stringResource(R.string.car),
+                    modifier = Modifier.size(40.dp),
+                    contentScale = ContentScale.Fit,
+                )
+            } else {
+                AsyncImage(
+                    logo,
+                    contentDescription = stringResource(R.string.car),
+                    modifier = Modifier.size(40.dp),
+                    contentScale = ContentScale.Fit,
+                )
+            }
+        }
     }
 }
--- app/src/main/java/io/github/patrickvillarroel/wheel/vault/ui/screen/component/CarCard.kt
+++ app/src/main/java/io/github/patrickvillarroel/wheel/vault/ui/screen/component/CarCard.kt
@@ -1,6 +1,5 @@
 package io.github.patrickvillarroel.wheel.vault.ui.screen.component
 
-import androidx.annotation.DrawableRes
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,24 +10,37 @@ import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import io.github.patrickvillarroel.wheel.vault.R
 
 @Composable
-fun CarCard(@DrawableRes imageRes: Int, modifier: Modifier = Modifier) {
+fun CarCard(image: Any, onClick: () -> Unit, modifier: Modifier = Modifier) {
     Card(
         modifier = modifier
             .size(80.dp)
             .border(2.dp, Color.Red, RoundedCornerShape(12.dp)),
+        onClick = onClick,
         shape = RoundedCornerShape(12.dp),
         colors = CardDefaults.cardColors(containerColor = Color.White),
     ) {
-        Image(
-            painter = painterResource(id = imageRes),
-            contentDescription = "Auto",
-            modifier = Modifier.fillMaxSize(),
-            contentScale = ContentScale.Fit,
-        )
+        if (image is Painter) {
+            Image(
+                painter = image,
+                contentDescription = stringResource(R.string.car),
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Fit,
+            )
+        } else {
+            AsyncImage(
+                model = image,
+                contentDescription = stringResource(R.string.car),
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Fit,
+            )
+        }
     }
 }
