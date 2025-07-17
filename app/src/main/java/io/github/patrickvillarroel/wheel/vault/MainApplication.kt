2911b50e11f26043f1d3a2bb8e2209cfeb6e0197
Rey <89825425+Kingg22@users.noreply.github.com>
2025-07-16 20:54:01 -0500

    feat(supabase): install plugins and setup coil with supabase storage

--- app/src/main/java/io/github/patrickvillarroel/wheel/vault/MainApplication.kt
+++ app/src/main/java/io/github/patrickvillarroel/wheel/vault/MainApplication.kt
@@ -1,19 +1,52 @@
 package io.github.patrickvillarroel.wheel.vault
 
 import android.app.Application
+import coil3.ImageLoader
+import coil3.PlatformContext
+import coil3.SingletonImageLoader
+import coil3.disk.DiskCache
+import coil3.disk.directory
+import coil3.memory.MemoryCache
+import coil3.network.ktor3.KtorNetworkFetcherFactory
+import io.github.jan.supabase.SupabaseClient
+import io.github.jan.supabase.annotations.SupabaseExperimental
+import io.github.jan.supabase.coil.coil3
 import io.github.patrickvillarroel.wheel.vault.di.wheelVaultModule
+import org.koin.android.ext.android.inject
 import org.koin.android.ext.koin.androidContext
 import org.koin.androix.startup.KoinStartup
 import org.koin.core.annotation.KoinExperimentalAPI
 import org.koin.dsl.KoinConfiguration
+import java.io.File
 
 @OptIn(KoinExperimentalAPI::class)
 class MainApplication :
     Application(),
-    KoinStartup {
+    KoinStartup,
+    SingletonImageLoader.Factory {
+    private val supabase: SupabaseClient by inject()
 
     override fun onKoinStartup() = KoinConfiguration {
         modules(wheelVaultModule)
         androidContext(this@MainApplication)
     }
+
+    @OptIn(SupabaseExperimental::class)
+    override fun newImageLoader(context: PlatformContext): ImageLoader = ImageLoader.Builder(context)
+        .components {
+            add(supabase.coil3)
+            add(KtorNetworkFetcherFactory())
+        }
+        .memoryCache {
+            MemoryCache.Builder()
+                .maxSizePercent(context, 0.25)
+                .build()
+        }
+        .diskCache {
+            DiskCache.Builder()
+                .directory(File(context.cacheDir, "image_cache"))
+                .maxSizeBytes(100L * 1024 * 1024)
+                .build()
+        }
+        .build()
 }
--- app/src/main/java/io/github/patrickvillarroel/wheel/vault/di/SupabaseModule.kt
+++ app/src/main/java/io/github/patrickvillarroel/wheel/vault/di/SupabaseModule.kt
@@ -2,7 +2,11 @@ package io.github.patrickvillarroel.wheel.vault.di
 
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.auth.ExternalAuthAction
+import io.github.jan.supabase.coil.Coil3Integration
 import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.postgrest.PropertyConversionMethod
+import io.github.jan.supabase.storage.Storage
 import io.github.patrickvillarroel.wheel.vault.BuildConfig
 import org.koin.dsl.module
 
@@ -17,6 +21,11 @@ val supabaseModule = module {
                 host = "wheel.supabase.com"
                 defaultExternalAuthAction = ExternalAuthAction.CustomTabs()
             }
+            install(Postgrest) {
+                propertyConversionMethod = PropertyConversionMethod.SERIAL_NAME
+            }
+            install(Storage)
+            install(Coil3Integration)
         }
     }
 }
