87788e23a245896b27c7c7218d35ab0b60ed7fc2
Rey <89825425+Kingg22@users.noreply.github.com>
2025-07-21 00:37:35 -0500

    feat(room): add room as local data source
    TODO connect datasource with dao

---
+++ app/src/main/java/io/github/patrickvillarroel/wheel/vault/data/dao/CarDao.kt
@@ -0,0 +1,6 @@
+package io.github.patrickvillarroel.wheel.vault.data.dao
+
+import androidx.room.Dao
+
+@Dao
+interface CarDao
---
+++ app/src/main/java/io/github/patrickvillarroel/wheel/vault/data/entity/CarFtsEntity.kt
@@ -0,0 +1,14 @@
+package io.github.patrickvillarroel.wheel.vault.data.entity
+
+import androidx.room.Entity
+import androidx.room.Fts4
+
+@Fts4(contentEntity = CarEntity::class)
+@Entity(tableName = "cars_fts")
+data class CarFtsEntity(
+    val model: String,
+    val brand: String,
+    val manufacturer: String,
+    val category: String?,
+    val description: String?,
+)
