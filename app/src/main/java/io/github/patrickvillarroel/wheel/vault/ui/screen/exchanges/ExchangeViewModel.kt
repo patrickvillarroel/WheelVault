49b446baacd1afd0ee5de89ad6d86826d6cc8166
Rey <89825425+Kingg22@users.noreply.github.com>
2025-09-24 15:50:43 -0500

    feat(exchanges): add temporal confirm state on VM and methods
    TODO implement methods

--- app/src/main/java/io/github/patrickvillarroel/wheel/vault/ui/screen/exchanges/ExchangeViewModel.kt
+++ app/src/main/java/io/github/patrickvillarroel/wheel/vault/ui/screen/exchanges/ExchangeViewModel.kt
@@ -6,9 +6,13 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.github.patrickvillarroel.wheel.vault.domain.model.CarItem
 import io.github.patrickvillarroel.wheel.vault.ui.screen.garage.GarageViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.update
+import java.util.UUID
 
 /* Temporal TODO change datasource of data */
 class ExchangeViewModel(garageViewModel: GarageViewModel) : ViewModel() {
@@ -20,8 +24,27 @@ class ExchangeViewModel(garageViewModel: GarageViewModel) : ViewModel() {
             is GarageViewModel.GarageUiState.Success -> ExchangeUiState.Success(it.cars)
         }
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), ExchangeUiState.Loading)
+    private val _exchangeConfirmState = MutableStateFlow<ExchangeConfirmUiState>(ExchangeConfirmUiState.Loading)
+    val exchangeConfirmState = _exchangeConfirmState.asStateFlow()
 
     fun exchangeCar(carItem: CarItem) {
+        /* TODO */
+    }
+
+    fun offersOf(carId: UUID) {
+        _exchangeConfirmState.update {
+            ExchangeConfirmUiState.WaitingConfirm(
+                (exchangeState.value as ExchangeUiState.Success).cars.first { it.id == carId },
+                (exchangeState.value as ExchangeUiState.Success).cars.random(),
+            )
+        }
+    }
+
+    fun rejectExchange(offeredCar: CarItem, requestedCar: CarItem) {
+        /* TODO */
+    }
+
+    fun acceptExchange(offeredCar: CarItem, requestedCar: CarItem) {
         /* TODO */
     }
 
@@ -31,5 +54,15 @@ class ExchangeViewModel(garageViewModel: GarageViewModel) : ViewModel() {
         @Immutable
         class Success(@Stable val cars: List<CarItem>) : ExchangeUiState
         object Error : ExchangeUiState
+    }
+
+    sealed interface ExchangeConfirmUiState {
+        object Loading : ExchangeConfirmUiState
+
+        @Immutable
+        class WaitingConfirm(val offeredCar: CarItem, val requestedCar: CarItem) : ExchangeConfirmUiState
+        object Accepted : ExchangeConfirmUiState
+        object Rejected : ExchangeConfirmUiState
+        object Error : ExchangeConfirmUiState
     }
 }
