2ce01968ded96343b2ce96577d181c07e60c9239
Rey <89825425+Kingg22@users.noreply.github.com>
2025-09-27 15:07:04 -0500

    feat(brand room): implement fetchAll and saveAll on datasource

--- app/src/main/java/io/github/patrickvillarroel/wheel/vault/data/datasource/room/BrandRoomDataSource.kt
+++ app/src/main/java/io/github/patrickvillarroel/wheel/vault/data/datasource/room/BrandRoomDataSource.kt
@@ -1,17 +1,20 @@
 package io.github.patrickvillarroel.wheel.vault.data.datasource.room
 
 import io.github.patrickvillarroel.wheel.vault.data.dao.BrandDao
+import io.github.patrickvillarroel.wheel.vault.data.datasource.image.ImageRepository
+import io.github.patrickvillarroel.wheel.vault.data.entity.toDomain
+import io.github.patrickvillarroel.wheel.vault.data.entity.toEntity
 import io.github.patrickvillarroel.wheel.vault.domain.model.Brand
 import io.github.patrickvillarroel.wheel.vault.domain.repository.BrandRepository
 import java.util.UUID
 
-class BrandRoomDataSource(private val dao: BrandDao) : BrandRepository {
+class BrandRoomDataSource(private val dao: BrandDao, private val imageRepository: ImageRepository) : BrandRepository {
     override suspend fun search(query: String): List<Brand> {
         TODO()
     }
 
-    override suspend fun fetchAll(): List<Brand> {
-        TODO()
+    override suspend fun fetchAll(forceRefresh: Boolean): List<Brand> = dao.fetchAll().map {
+        it.toDomain(imageRepository.loadImage(it.id) ?: Brand.DEFAULT_IMAGE)
     }
 
     override suspend fun fetch(id: UUID): Brand? {
@@ -25,7 +28,11 @@ class BrandRoomDataSource(private val dao: BrandDao) : BrandRepository {
     override suspend fun fetchByDescription(description: String): Brand? {
         TODO()
     }
-    suspend fun insert(brand: Brand): Brand {
-        TODO()
+
+    suspend fun saveAll(brands: List<Brand>, images: List<ByteArray>) {
+        dao.insertAllBrands(brands.map { it.toEntity() })
+        brands.zip(images).forEach { (brand, image) ->
+            imageRepository.saveImage(brand.id.toString(), image)
+        }
     }
 }
