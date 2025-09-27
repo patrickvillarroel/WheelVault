02d1a7fcf8229ecb46eaf1e4101a216e216dd9a4
Rey <89825425+Kingg22@users.noreply.github.com>
2025-09-27 11:58:01 -0500

    fix(news VM): reduce log message and filter stateflow, remove data of classes

--- app/src/main/java/io/github/patrickvillarroel/wheel/vault/ui/screen/home/HomeViewModel.kt
+++ app/src/main/java/io/github/patrickvillarroel/wheel/vault/ui/screen/home/HomeViewModel.kt
@@ -9,16 +9,18 @@ import io.github.patrickvillarroel.wheel.vault.domain.model.VideoNews
 import io.github.patrickvillarroel.wheel.vault.domain.usecase.GetVideosNewsUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.filterIsInstance
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-data class HomeViewModel(private val getVideosNewsUseCase: GetVideosNewsUseCase) : ViewModel() {
+class HomeViewModel(private val getVideosNewsUseCase: GetVideosNewsUseCase) : ViewModel() {
     private val newsUiState = MutableStateFlow<NewsUiState>(NewsUiState.Loading)
-    val news = newsUiState.map {
-        (it as? NewsUiState.Success)?.news ?: emptyList()
-    }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
+    val news = newsUiState
+        .filterIsInstance<NewsUiState.Success>()
+        .map { it.news }
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), emptyList())
 
     init {
         fetchNews()
@@ -26,17 +28,19 @@ data class HomeViewModel(private val getVideosNewsUseCase: GetVideosNewsUseCase)
 
     fun fetchNews() {
         viewModelScope.launch {
+            newsUiState.update { NewsUiState.Loading }
             Log.i("HomeViewModel", "Fetching news...")
             val videos = getVideosNewsUseCase.getVideos()
-            Log.i("HomeViewModel", "Fetched news: $videos")
-            newsUiState.update { NewsUiState.Success(videos) }
+            Log.i("HomeViewModel", "Fetched ${videos.size} news")
+            val status = NewsUiState.Success(videos)
+            newsUiState.update { status }
         }
     }
 
     sealed interface NewsUiState {
-        data object Loading : NewsUiState
+        object Loading : NewsUiState
 
         @Immutable
-        data class Success(@Stable val news: List<VideoNews>) : NewsUiState
+        class Success(@Stable val news: List<VideoNews>) : NewsUiState
     }
 }
