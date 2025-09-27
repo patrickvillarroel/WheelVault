162f33783f7c9ef784697a872d483d3265c1399b
Rey <89825425+Kingg22@users.noreply.github.com>
2025-09-27 15:03:49 -0500

    feat(data): add SyncMediator

---
+++ app/src/main/java/io/github/patrickvillarroel/wheel/vault/data/SyncMediator.kt
@@ -0,0 +1,58 @@
+@file:OptIn(ExperimentalContracts::class)
+
+package io.github.patrickvillarroel.wheel.vault.data
+
+import kotlin.contracts.ExperimentalContracts
+import kotlin.contracts.InvocationKind
+import kotlin.contracts.contract
+
+object SyncMediator {
+    suspend inline fun <T : Any> fetch(
+        localFetch: suspend () -> T?,
+        remoteFetch: suspend () -> T?,
+        saveRemote: suspend (T?) -> Unit,
+        forceRefresh: Boolean,
+    ): T? {
+        contract {
+            callsInPlace(localFetch, InvocationKind.AT_MOST_ONCE)
+            callsInPlace(remoteFetch, InvocationKind.AT_MOST_ONCE)
+            callsInPlace(saveRemote, InvocationKind.AT_MOST_ONCE)
+        }
+        return if (forceRefresh) {
+            val remoteData = remoteFetch()
+            saveRemote(remoteData)
+            remoteData
+        } else {
+            localFetch() ?: run {
+                val remoteData = remoteFetch()
+                saveRemote(remoteData)
+                remoteData
+            }
+        }
+    }
+
+    suspend inline fun <T : Any> fetchList(
+        localFetch: suspend () -> List<T>,
+        remoteFetch: suspend () -> List<T>,
+        saveRemote: suspend (List<T>) -> Unit,
+        forceRefresh: Boolean = false,
+    ): List<T> {
+        contract {
+            callsInPlace(localFetch, InvocationKind.AT_MOST_ONCE)
+            callsInPlace(remoteFetch, InvocationKind.AT_MOST_ONCE)
+            callsInPlace(saveRemote, InvocationKind.AT_MOST_ONCE)
+        }
+        return if (forceRefresh) {
+            val remoteData = remoteFetch()
+            saveRemote(remoteData)
+            remoteData
+        } else {
+            val localData = localFetch()
+            localData.ifEmpty {
+                val remoteData = remoteFetch()
+                saveRemote(remoteData)
+                remoteData
+            }
+        }
+    }
+}
