e14600b13a8d69f60f028cbb93a7f03f49db2d99
Rey <89825425+kingg22@users.noreply.github.com>
2025-10-31 09:36:58 -0500

    fix(room): remove default values and use current timestamp db function

--- app/src/main/kotlin/io/github/patrickvillarroel/wheel/vault/data/entity/BrandEntity.kt
+++ app/src/main/kotlin/io/github/patrickvillarroel/wheel/vault/data/entity/BrandEntity.kt
@@ -3,13 +3,13 @@ package io.github.patrickvillarroel.wheel.vault.data.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import kotlin.uuid.Uuid
 
 @Entity(tableName = "brands")
 class BrandEntity(
-    @PrimaryKey val id: String = Uuid.random().toString(),
+    @PrimaryKey
+    val id: String,
     val name: String,
     val description: String,
-    @ColumnInfo(name = "created_at")
-    val createdAt: Long = System.currentTimeMillis(),
+    @ColumnInfo(name = "created_at", defaultValue = "CURRENT_TIMESTAMP", index = true)
+    val createdAt: Long? = null,
 )
