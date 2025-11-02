90780a29a11432b90801e9df46afdbd3bd634e1c
Rey <89825425+kingg22@users.noreply.github.com>
2025-11-02 10:27:56 -0500

    feat(brand repository): add fetch names method

--- app/src/main/kotlin/io/github/patrickvillarroel/wheel/vault/data/datasource/supabase/BrandSupabaseDataSource.kt
+++ app/src/main/kotlin/io/github/patrickvillarroel/wheel/vault/data/datasource/supabase/BrandSupabaseDataSource.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import coil3.request.ImageRequest
 import io.github.jan.supabase.SupabaseClient
 import io.github.jan.supabase.postgrest.from
+import io.github.jan.supabase.postgrest.query.Columns
+import io.github.jan.supabase.postgrest.query.Order
 import io.github.jan.supabase.storage.authenticatedStorageItem
 import io.github.patrickvillarroel.wheel.vault.data.objects.BrandObj
 import io.github.patrickvillarroel.wheel.vault.data.objects.toDomain
@@ -22,28 +24,26 @@ class BrandSupabaseDataSource(private val supabase: SupabaseClient, private val 
         }
     }.decodeList<BrandObj>().map { it.toDomain(fetchImage(it.id!!)) }
 
-    override suspend fun fetchAll(forceRefresh: Boolean): List<Brand> =
-        supabase.from(BrandObj.TABLE).select().decodeList<BrandObj>().map {
-            it.toDomain(fetchImage(it.id!!))
-        }
+    override suspend fun fetchAllNames(forceRefresh: Boolean): List<String> =
+        supabase.from(BrandObj.TABLE).select(Columns.list("name")) {
+            order("created_at", Order.DESCENDING)
+        }.decodeList<Map<String, String>>().flatMap { it.values }
+
+    override suspend fun fetchAll(forceRefresh: Boolean): List<Brand> = supabase.from(BrandObj.TABLE).select {
+        order("created_at", Order.DESCENDING)
+    }.decodeList<BrandObj>().map { it.toDomain(fetchImage(it.id!!)) }
 
     override suspend fun fetch(id: Uuid): Brand? = supabase.from(BrandObj.TABLE).select {
         filter { eq("id", id) }
-    }.decodeSingleOrNull<BrandObj>()?.toDomain {
-        fetchImage(it.id!!)
-    }
+    }.decodeSingleOrNull<BrandObj>()?.toDomain { fetchImage(it.id!!) }
 
     override suspend fun fetchByName(name: String): Brand? = supabase.from(BrandObj.TABLE).select {
         filter { ilike("name", "%$name%") }
-    }.decodeSingleOrNull<BrandObj>()?.toDomain {
-        fetchImage(it.id!!)
-    }
+    }.decodeSingleOrNull<BrandObj>()?.toDomain { fetchImage(it.id!!) }
 
     override suspend fun fetchByDescription(description: String): Brand? = supabase.from(BrandObj.TABLE).select {
         filter { ilike("description", "%$description%") }
-    }.decodeSingleOrNull<BrandObj>()?.toDomain {
-        fetchImage(it.id!!)
-    }
+    }.decodeSingleOrNull<BrandObj>()?.toDomain { fetchImage(it.id!!) }
 
     private fun fetchImage(id: Uuid, contentType: String = "png") = ImageRequest.Builder(context)
         .data(authenticatedStorageItem(BrandObj.BUCKET_IMAGES, "$id.$contentType"))
