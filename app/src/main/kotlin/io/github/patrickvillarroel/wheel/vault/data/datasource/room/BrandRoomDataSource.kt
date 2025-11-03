07e936aa195816a0ca56ea13a0a6377d2e0c67ce
Rey <89825425+kingg22@users.noreply.github.com>
2025-11-02 22:30:33 -0500

    fix(brand room repository): put extension file before save image

--- app/src/main/kotlin/io/github/patrickvillarroel/wheel/vault/data/datasource/room/BrandRoomDataSource.kt
+++ app/src/main/kotlin/io/github/patrickvillarroel/wheel/vault/data/datasource/room/BrandRoomDataSource.kt
@@ -9,31 +9,35 @@ import io.github.patrickvillarroel.wheel.vault.domain.repository.BrandRepository
 import kotlin.uuid.Uuid
 
 class BrandRoomDataSource(private val dao: BrandDao, private val imageRepository: ImageRepository) : BrandRepository {
-    override suspend fun fetchAll(forceRefresh: Boolean): List<Brand> = dao.fetchAll().map {
-        it.toDomain(imageRepository.loadImage(it.id) ?: Brand.DEFAULT_IMAGE)
+    override suspend fun fetchAll(forceRefresh: Boolean): List<Brand> = dao.fetchAll().map { brandEntity ->
+        brandEntity.toDomain(imageRepository.loadImage(brandEntity.id + ".png") ?: Brand.DEFAULT_IMAGE)
     }
 
     override suspend fun fetch(id: Uuid, forceRefresh: Boolean): Brand? = dao.fetchById(id.toString())?.let { entity ->
-        entity.toDomain(imageRepository.loadImage(entity.id) ?: Brand.DEFAULT_IMAGE)
+        entity.toDomain(imageRepository.loadImage(entity.id + ".png") ?: Brand.DEFAULT_IMAGE)
     }
 
-    override suspend fun search(query: String): List<Brand> = dao.search(query).map {
-        it.toDomain(imageRepository.loadImage(it.id) ?: Brand.DEFAULT_IMAGE)
+    override suspend fun search(query: String): List<Brand> = dao.search(query).map { brandEntity ->
+        brandEntity.toDomain(imageRepository.loadImage(brandEntity.id + ".png") ?: Brand.DEFAULT_IMAGE)
     }
 
     override suspend fun fetchAllNames(forceRefresh: Boolean): List<String> = dao.fetchAllNames()
-
-    override suspend fun fetchByName(name: String): Brand? = dao.fetchByName(name)?.let {
-        it.toDomain(imageRepository.loadImage(it.id) ?: Brand.DEFAULT_IMAGE)
+    override suspend fun fetchAllImages(forceRefresh: Boolean): Map<Uuid, Any> = dao.fetchAllIds().associate { id ->
+        Uuid.parse(id) to (imageRepository.loadImage("$id.png")?.first() ?: Brand.DEFAULT_IMAGE)
     }
 
-    override suspend fun fetchByDescription(description: String): Brand? = dao.fetchByDescription(description)?.let {
-        it.toDomain(imageRepository.loadImage(it.id) ?: Brand.DEFAULT_IMAGE)
+    override suspend fun fetchByName(name: String): Brand? = dao.fetchByName(name)?.let { brandEntity ->
+        brandEntity.toDomain(imageRepository.loadImage(brandEntity.id + ".png") ?: Brand.DEFAULT_IMAGE)
     }
+
+    override suspend fun fetchByDescription(description: String): Brand? =
+        dao.fetchByDescription(description)?.let { brandEntity ->
+            brandEntity.toDomain(imageRepository.loadImage(brandEntity.id + ".png") ?: Brand.DEFAULT_IMAGE)
+        }
 
     suspend fun save(brand: Brand, image: ByteArray?) {
         dao.insertBrand(brand.toEntity())
-        if (image != null) imageRepository.saveImage(brand.id.toString(), image)
+        if (image != null) imageRepository.saveImage(brand.id.toString() + ".png", image)
     }
 
     suspend fun saveAll(brands: List<Brand>, images: List<ByteArray>) {
