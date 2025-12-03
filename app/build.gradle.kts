a2dea436fa56a81c3c073261c794419f38beedf3
Rey <89825425+kingg22@users.noreply.github.com>
2025-12-02 22:08:45 -0500

    build: add kover to test coverage measure

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+    alias(libs.plugins.kover)
     alias(libs.plugins.ksp)
     alias(libs.plugins.ktlint)
 }
